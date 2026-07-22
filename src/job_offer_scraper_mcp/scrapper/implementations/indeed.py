866faaaf7c1ad2501ded4c935756cd44aa4fc96f
JuanjoLopez19 <juanjo.lopez.gomez.19@gmail.com>
2026-07-22 18:32:13 +0200

    [FEAT] Enhance IndeedScrapper with structured data extraction
    - Added methods to extract job title, company, location, description, and criteria from structured JSON data in HTML.
    - Implemented a mapping for job types and improved handling of salary information.
    - Updated unit tests to validate new extraction functionalities and handle cases with missing data.
    - Added a new entry to .gitignore for downloaded files.

--- src/job_offer_scraper_mcp/scrapper/implementations/indeed.py
+++ src/job_offer_scraper_mcp/scrapper/implementations/indeed.py
@@ -1,3 +1,4 @@
+import json
 from dataclasses import dataclass
 from sys import argv
 from typing import cast
@@ -12,38 +13,89 @@ from job_offer_scraper_mcp.scrapper.config import SupportedSites
 @dataclass(slots=True)
 class IndeedScrapper(JobOfferExtractor):
     type: str = SupportedSites.INDEED.value
-    description: str | None = None
-    criteria: str | None = None
+
+    job_type_map = {
+        "FULL_TIME": "Full-time",
+        "PART_TIME": "Part-time",
+        "CONTRACT": "Contract",
+        "TEMPORARY": "Temporary",
+        "INTERN": "Intern",
+    }
 
     def find_job_offer_info(self, html: bs):
-        info = html.find("div", class_="jobsearch-JobComponent")
-
-        return info
+        return html
 
     def find_job_description(self, html: bs):
-        container = html.find("div", {"class": "jobsearch-JobComponent-description"})
 
-        if container is None:
+        data = self.__load_json_data(html)
+        if data is None:
             return None
-
-        container = cast(Tag, container)
-        description = container.find("div", {"id": "jobDescriptionText"})
-        if description is None:
-            return None
-        return description.text.strip()
+        return data.get("description", "No description available")
 
     def find_job_criteria(self, html: bs, **kwargs):
-        return None
-        # header = job_offer.find(
-        #     "div",
-        #     {"class": "jobsearch-InfoHeaderContainer jobsearch-DesktopStickyContainer"},
-        # )
+        data = self.__load_json_data(html)
+        if data is None:
+            return None
 
-        # header_container = header.find(
-        #     "div", {"data-testid": "jobsearch-CompanyInfoContainer"}
-        # )
+        criteria_list = []
+        if "employmentType" in data:
+            employment_type = self.job_type_map.get(
+                data["employmentType"][0], "Unknown"
+            )
+            criteria_list.append(f"Employment type: {employment_type}")
 
-        # print(header_container.text.strip().split("\n"))
+        if "baseSalary" in data:
+            base_salary = data["baseSalary"]["value"]
+            if "maxValue" in base_salary:
+                max_base_salary = base_salary["maxValue"]
+            else:
+                max_base_salary = "Not specified"
+            if "minValue" in base_salary:
+                min_base_salary = base_salary["minValue"]
+            else:
+                min_base_salary = "Not specified"
+            criteria_list.append(f"Base salary:{min_base_salary}-{max_base_salary}")
+
+        if "jobLocationType" in data:
+            job_location_type = data["jobLocationType"]
+            criteria_list.append(f"Job location type: {job_location_type}")
+
+        if "applicantLocationRequirements" in data:
+            applicant_location_requirements = data["applicantLocationRequirements"]
+            criteria_list.append(
+                f"Applicant location requirements: {json.dumps(applicant_location_requirements)}"
+            )
+
+        return "\n".join(criteria_list)
+
+    def find_job_title(self, html: bs):
+        data = self.__load_json_data(html)
+        if data is None:
+            return None
+        return data.get("title", "No title available")
+
+    def find_job_company(self, html: bs):
+        data = self.__load_json_data(html)
+        if data is None:
+            return None
+
+        if "hiringOrganization" in data:
+            return data["hiringOrganization"]["name"]
+
+    def find_job_location(self, html: bs):
+        data = self.__load_json_data(html)
+        if data is None:
+            return None
+
+        if "jobLocation" in data:
+            return data["jobLocation"]["address"]["addressLocality"]
+
+    def __load_json_data(self, html: bs) -> dict | None:
+        script = html.find("script", type="application/ld+json")
+        if script is None:
+            return None
+
+        return json.loads(cast(Tag, script).string or "")
 
 
 if __name__ == "__main__":
@@ -55,5 +107,4 @@ if __name__ == "__main__":
 
     scrapper = FactoryScrapper.get_scrapper(url)
     scrapper.extract()
-    print(scrapper.get_job_description())
-    print(scrapper.get_job_criteria())
+    print(scrapper.get_job_offer_info())
